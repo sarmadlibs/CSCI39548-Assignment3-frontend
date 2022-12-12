0f86afd0e006315c462c6e76be6f03247a690c16
Sarmad <afzalsarmad77@gmail.com>
2022-12-11 19:11:36 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./App.css";
+
+//Router
+import { Switch, Route } from "react-router-dom";
+//Components
+import {
+  HomePageContainer,
+  InstructorContainer,
+  CourseContainer,
+  AllInstructorsContainer,
+  AllCoursesContainer,
+  NewCourseContainer,
+  EditCourseContainer
+} from './components/containers';
+
+// if you create separate components for adding/editing 
+// a student or instructor, make sure you add routes to those
+// components here
+
+const App = () => {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={HomePageContainer} />
+        <Route exact path="/instructors" component={AllInstructorsContainer} />
+        <Route exact path="/instructor/:id" component={InstructorContainer} />
+        <Route exact path="/courses" component={AllCoursesContainer} />
+        <Route exact path="/newcourse" component={NewCourseContainer} />
+        <Route exact path="/course/:id" component={CourseContainer} />
+        <Route exact path="/editcourse/:id" component={EditCourseContainer} />
+
+      </Switch>        
+    </div>
+  );
+}
+
+export default App;
+
